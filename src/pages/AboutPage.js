5c04b98fec74d0ae0a7b17e01aa8a1afa1668661
Rodrigo Martinez <rodrigomartinez.schmidt@gmail.com>
2023-03-10 19:02:46 +0100

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
+
+const AboutPage = () => {
+	return (
+		<main>
+			<PageHero title='about' />
+			<Wrapper className='page section section-center'>
+				<img src={aboutImg} alt='nice desk' />
+				<article>
+					<div className='title'>
+						<h2>our story</h2>
+						<div className='underline'></div>
+					</div>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+						officia facilis officiis vitae ullam voluptate at ea quaerat. Esse
+						odit labore corrupti error ipsum enim ratione quas impedit nulla.
+						Inventore, perspiciatis illo aperiam ut delectus iusto? Enim, saepe
+						vel ut at quae nemo accusamus necessitatibus maiores perferendis
+						vero suscipit cupiditate.
+					</p>
+				</article>
+			</Wrapper>
+		</main>
+	);
+};
+
+const Wrapper = styled.section`
+	display: grid;
+	gap: 4rem;
+	img {
+		width: 100%;
+		display: block;
+		border-radius: var(--radius);
+		height: 500px;
+		object-fit: cover;
+	}
+	p {
+		line-height: 2;
+		max-width: 45em;
+		margin: 0 auto;
+		margin-top: 2rem;
+		color: var(--clr-grey-5);
+	}
+	.title {
+		text-align: left;
+	}
+	.underline {
+		margin-left: 0;
+	}
+	@media (min-width: 992px) {
+		grid-template-columns: 1fr 1fr;
+	}
+`;
+export default AboutPage;
